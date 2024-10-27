d021f2c81bb32d1444893e62ccf9b76765f961da
Matt Kramer <mjkramer@users.noreply.github.com>
2024-10-26 23:54:52 -0700

    Update flow_charge_data.sh

--- actions/flow_charge_data.sh
+++ actions/flow_charge_data.sh
@@ -22,17 +22,17 @@ get_outpath() {
 flowpath=$(get_outpath "$data_outbase" FLOW.hdf5)
 logpath=$(get_outpath "$log_outbase" log)
 
-if [[ "$(stat -c %s "$inpath")" -gt 1000000000 ]]; then
-    echo "File is larger than 1 GB; bailing"
+if [[ "$(stat -c %s "$inpath")" -gt 4000000000 ]]; then
+    echo "File is larger than 4 GB; bailing"
     exit 1
 fi
 
 cd $ROOT_OF_ARCUBE_NEARLINE/_install/ndlar_flow
 
-workflow1='yamls/proto_nd_flow/workflows/charge/charge_event_building.yaml'
-workflow2='yamls/proto_nd_flow/workflows/charge/charge_event_reconstruction.yaml'
-workflow3='yamls/proto_nd_flow/workflows/combined/combined_reconstruction.yaml'
-workflow4='yamls/proto_nd_flow/workflows/charge/prompt_calibration.yaml'
+workflow1='yamls/fsd_flow/workflows/charge/charge_event_building.yaml'
+workflow2='yamls/fsd_flow/workflows/charge/charge_event_reconstruction.yaml'
+workflow3='yamls/fsd_flow/workflows/combined/combined_reconstruction.yaml'
+workflow4='yamls/fsd_flow/workflows/charge/prompt_calibration.yaml'
 # workflow5='yamls/proto_nd_flow/workflows/charge/final_calibration.yaml'
 
 rm -f "$flowpath"
