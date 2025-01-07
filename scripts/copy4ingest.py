8bcfdec6a012295ef6071c3f52962abfd6413e5c
Matt Kramer <mjkramer@users.noreply.github.com>
2025-01-06 19:08:35 -0800

    Add copy4ingest.py

---
+++ scripts/copy4ingest.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import os
+from pathlib import Path
+import shutil
+
+
+def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument('--indir', type=Path, required=True)
+    ap.add_argument('--outdir', type=Path, required=True)
+    args = ap.parse_args()
+
+    for jsonpath in args.indir.rglob('*.json'):
+        datapath = jsonpath.parent / jsonpath.stem
+
+        with open(jsonpath) as f:
+            meta = json.load(f)
+
+        assert meta['name'] == datapath.name
+
+        # .h5 -> .hdf5, if applicable
+        suffix = datapath.suffix
+        if suffix == '.h5':
+            suffix = '.hdf5'
+        meta['name'] = datapath.with_suffix(suffix)
+
+        out_datapath = \
+            args.outdir / datapath.relative_to(args.indir).with_suffix(suffix)
+
+        os.mkdir(out_datapath.parent)
+        print(f'COPY {datapath} --> {out_datapath}')
+        shutil.copy(datapath, out_datapath)
+
+        out_jsonpath = out_datapath.with_suffix(out_datapath.suffix + '.json')
+        print(f'WRITE {out_jsonpath}\n')
+        with open(out_jsonpath, 'w') as f:
+            json.dump(meta, f)
+
+
+if __name__ == '__main__':
+    main()
