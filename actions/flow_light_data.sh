2dc15b7ba1624cd641b0af770be97593858711aa
Angela White <123964490+AWh1t3@users.noreply.github.com>
2026-06-11 09:18:31 -0500

    Updated light flowing script for sortable file name
    Also updated for Run 3 directory: tried to do this in terminal but ran into a permissions issue, apologies!

--- actions/flow_light_data.sh
+++ actions/flow_light_data.sh
@@ -4,10 +4,11 @@ stage=flowed_light
 
 source $(dirname $BASH_SOURCE)/../lib/init.inc.sh
 
-inname=${ARCUBE_NEARLINE_DATA_DIR_NAME:-LRS_run2}
+inname=${ARCUBE_NEARLINE_DATA_DIR_NAME:-LRS_run3}
 inbase=$data_root/$inname
 
 inpath=$1; shift
+outstamp=$(TZ=America/Chicago date --date=@$(jq '.metadata."core.start_time"' $inpath.json) +%Y_%m_%d_%H_%S)
 
 get_outpath() {
     outbase=$1
@@ -15,7 +16,15 @@ get_outpath() {
 
     indir=$(dirname "$inpath")
     reldir=$(echo "$indir" | sed "s|^$inbase/||")
-    outname=$(basename "$inpath" .data).$ext
+    inbase_name=$(basename "$inpath")
+    if [[ "$inbase_name" =~ ^mpd_(.+)_rctl_([0-9]+)_p([0-9]+)\.data$ ]]; then
+        config="${BASH_REMATCH[1]}"
+        run=$(printf "%06d" "${BASH_REMATCH[2]}")
+        subrun=$(printf "%05d" "${BASH_REMATCH[3]}")
+        outname="mpd_${config}_${outstamp}_${run}_p${subrun}.$ext"
+    else
+        outname=$(basename "$inpath" .data).$ext
+    fi
     mkdir -p "$outbase/$reldir"
     realpath "$outbase/$reldir/$outname"
 }
